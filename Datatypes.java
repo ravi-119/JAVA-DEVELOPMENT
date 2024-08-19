8e6393b23fdd6a8a7002d75994a1c2ca1845b4b0
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2024-08-19 13:16:44 -0400

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,24 @@
+class Datatypes {
+    public static void main(String[] args) {
+
+        // Datatypes in Java
+        int num1 = 10;
+        byte by = 127;
+        short sh = 558;
+        long l = 58584L;
+        float f = 5.85f;
+        double d = 5.88;
+        char c = 'R';
+        boolean  b = true;
+
+        System.out.println(num1);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,18 @@
+class Variable {
+    public static void main(String[] args) {
+
+        // variable in java 
+
+        System.out.println(3+5);
+        System.out.println(8+7);   
+
+        //  int num1 = 3;
+        //  int num2 = 8;
+        //  System.out.println(num1 + num2);
+
+         int num1 = 5;
+         int num2 = 10;
+         int result = num1 + num2;
+         System.out.println(result); 
+    }
+}
